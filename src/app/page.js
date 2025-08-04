01e72cb6efa135d31b29d112922d2c133ad56b1c
hamzahashmi033 <hashmihamza846@gmail.com>
2025-08-04 22:26:52 +0500

    fixed conflicts and merged all code together

--- src/app/page.js
+++ src/app/page.js
@@ -10,11 +10,7 @@ import Map from '@/widgets/Map';
 import Section2 from '@/widgets/Section2';
 import Section3 from '@/widgets/Section3';
 import About from '@/widgets/AboutUs';
-import Header from "@/widgets/Header";
-import Banner from "@/widgets/Banner";
 import "./styles.css";
-import ScrollContext from "@/context/SmoothScrollContext";
-import CoreServicesPreview from "@/widgets/CoreServicesPreview";
 import DigitalSlider from "@/widgets/Digital and operational transformation";
 import ContactSection from "@/widgets/ContactUs";
 
